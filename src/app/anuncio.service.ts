a593c46ea20848a6aee8cc3e51b19fa2d31a0db2
ERICK1800 <ericklimaflorencio@gmail.com>
2023-06-13 09:46:55 -0300

    Implementar o cadastro de Anúncios

--- src/app/anuncio.service.ts
+++ src/app/anuncio.service.ts
@@ -1,9 +1,22 @@
+import { Anuncio } from './anuncio';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnuncioService {
 
-  constructor() { }
+  url = "http://localhost:3000/anuncios";
+
+  constructor(private http: HttpClient) { }
+
+  getAnuncio(): Observable<Anuncio[]>{
+    return this.http.get<Anuncio[]>(this.url);
+  }
+
+  save(anuncio: Anuncio): Observable<Anuncio>{
+    return this.http.post<Anuncio>(this.url, anuncio);
+  }
 }
--- src/app/anuncio/anuncio.component.ts
+++ src/app/anuncio/anuncio.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Anuncio } from '../anuncio';
+import { AnuncioService } from '../anuncio.service';
 
 @Component({
   selector: 'app-anuncio',
   templateUrl: './anuncio.component.html',
   styleUrls: ['./anuncio.component.css']
 })
-export class AnuncioComponent {
+export class AnuncioComponent  implements OnInit{
+
+  anuncios: Anuncio[] = [];
+  formGroupAnuncio : FormGroup;
+
+  constructor(private  anuncioService: AnuncioService, private formBuilder: FormBuilder){
+    this.formGroupAnuncio = formBuilder.group({
+      id : [''],
+      img: [''],
+      titulo: [''],
+      textA: [''],
+      preco: ['']
+    })
+  }
+
+  ngOnInit(): void {
+    this.LoadClient();
+  }
+
+  LoadClient(){
+    this.anuncioService.getAnuncio().subscribe({
+      next: data => this.anuncios = data,
+      error: msg => console.log("Erro ao chamar o endpont " + msg)
+    });
+  }
+
+  save(){
+    this.anuncioService.save(this.formGroupAnuncio.value).subscribe({
+      next: data => {
+        this.anuncios.push(data);
+        this.formGroupAnuncio.reset();
+      }
+    })
+  }
 
 }
